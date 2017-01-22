1bfd6240c52ec5d8e5ca29ce2d515cc0234fce61
abiralsambhu <abiralsambhu@gmail.com>
2017-01-22 22:49:41 +0545

    insertion sort  implementation as #100daysofcode Day 6

---
+++ insertionsort.py
@@ -0,0 +1,18 @@
+##always maintaining a sorted sublist in the lower positions of the list. Each new item is then inserted back into the previous sorted sublist such that the sorted sublist is one item max than current
+
+
+def insertionSort(lst):
+    for index in range(1, len(lst)):
+        currentvalue = lst[index] 
+        currentposition = index
+        while currentposition > 0 and lst[currentposition - 1] > currentvalue:
+            lst[currentposition] = lst[currentposition - 1]
+            currentposition -=1
+
+        lst[currentposition] = currentvalue
+
+######implementation ############################
+
+lst = [33,54,23,13,45,78,88,56,90] #List defined for implementation
+insertionSort(lst)
+print(lst)
