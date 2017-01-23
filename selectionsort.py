83877e73a76c339bce8503c5a1460321742f94d2
abiralsambhu <abiralsambhu@gmail.com>
2017-01-23 22:38:27 +0545

    selection sort implementation day 7

---
+++ selectionsort.py
@@ -0,0 +1,21 @@
+import sys
+def selectionSort(lst):
+   for selectedLocation in range(len(lst)-1,0,-1):
+       maxPosition=0
+       for currentindex in range(1,selectedLocation+1): #since range(0,4) is exclusive to 4
+           if lst[currentindex]>lst[maxPosition]:
+               maxPosition = currentindex
+
+  
+       lst[selectedLocation], lst[maxPosition] = lst[maxPosition],lst[selectedLocation]
+      
+
+lst = []
+print ("input array size")
+i = int(input())
+for a in range(0,i):
+    ele= int(input(print("Enter element {0} ".format(a+1))))
+    lst.append(ele)
+
+selectionSort(lst)
+print(lst)
